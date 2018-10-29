b3b04c47dd90b55d92c40ed381646122161e7ea7
Igor Emets <i.yemets@p1k.co.uk>
2018-10-29 17:32:52 +0200

    Some optimizations

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@
 // });
 
 // setTimeout(() => {
-//     worker.postMessage({ kek: 1 });
+//     worker.postMessage({ foo: 1 });
 // }, 1000);
 
 const CELL_SIZE = 5; // px
@@ -13,6 +13,47 @@ const GRID_COLOR = "#CCCCCC";
 const DEAD_COLOR = "#FFFFFF";
 const ALIVE_COLOR = "#000000";
 
+
+const fps = new class {
+    fps: HTMLPreElement = document.querySelector("pre");
+    frames: number[] = [];
+    lastFrameTimeStamp = performance.now();
+
+    render() {
+        // Convert the delta time since the last frame render into a measure
+        // of frames per second.
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+        // Save only the latest 100 timings.
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        // Find the max, min, and mean of our 100 latest timings.
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum / this.frames.length;
+
+        // Render the statistics.
+        this.fps.textContent = `
+  Frames per Second:
+           latest = ${Math.round(fps)}
+  avg of last 100 = ${Math.round(mean)}
+  min of last 100 = ${Math.round(min)}
+  max of last 100 = ${Math.round(max)}
+  `.trim();
+    }
+};
 
 
 (async () => {
@@ -23,26 +64,46 @@ const ALIVE_COLOR = "#000000";
     // console.log(addOne(1, 2));
     // console.timeEnd('rust');
 
+    let frameId = null;
+
+    window.addEventListener('keyup', (e: KeyboardEvent) => {
+        if (e.key !== ' ') {
+            return;
+        }
+
+        if (frameId === null) {
+            frameId = requestAnimationFrame(renderLoop);
+        } else {
+            cancelAnimationFrame(frameId);
+            frameId = null;
+        }
+    });
+
     const universe = Universe.new();
     const width = universe.width();
     const height = universe.height();
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
 
     const canvas = document.querySelector<HTMLCanvasElement>('canvas');
     console.assert(canvas !== null);
+
     canvas.height = (CELL_SIZE + 1) * height + 1;
     canvas.width = (CELL_SIZE + 1) * width + 1;
 
     const ctx = canvas.getContext('2d');
 
-    const renderLoop = () => {
+    const renderLoop = (t: number) => {
+        fps.render();
         universe.tick();
 
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawGrid();
         drawCells();
 
-        requestAnimationFrame(renderLoop);
+        frameId = requestAnimationFrame(renderLoop);
     };
-    requestAnimationFrame(renderLoop);
+    // frameId = requestAnimationFrame(renderLoop);
 
     const drawGrid = () => {
         ctx.beginPath();
@@ -68,20 +129,18 @@ const ALIVE_COLOR = "#000000";
     };
 
     const drawCells = () => {
-        const cellsPtr = universe.cells();
-        const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
-
         ctx.beginPath();
+        ctx.fillStyle = ALIVE_COLOR;
 
         for (let row = 0; row < height; row++) {
             for (let col = 0; col < width; col++) {
                 const idx = getIndex(row, col);
 
-                ctx.fillStyle = cells[idx] === Cell.Dead
-                    ? DEAD_COLOR
-                    : ALIVE_COLOR;
+                if (cells[idx] === Cell.Dead) {
+                    continue;
+                }
 
-                ctx.fillRect(
+                ctx.rect(
                     col * (CELL_SIZE + 1) + 1,
                     row * (CELL_SIZE + 1) + 1,
                     CELL_SIZE,
@@ -90,6 +149,7 @@ const ALIVE_COLOR = "#000000";
             }
         }
 
-        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
     };
 })();
